370853962a34af4da0c74091efe6bd0a0dba9735
owell andry <owellandry10@gmail.com>
2024-03-01 15:19:37 -0500

    update changes page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,9 @@
-import { SpeedInsights } from "@vercel/speed-insights/next"
-import Head from 'next/head';
+
 import './css/index.css'
 
 export default function Home() {
   return (
     <div>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <SpeedInsights />
-      </Head>
       <div className="header">
         <h1>DOCUMENTACION DEL API <br />
           PROXIMAMENTE</h1>
